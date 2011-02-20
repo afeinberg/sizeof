873366c23aeb8664125945462793c7e08e48e20b
Jonathan Ellis <jbellis@gmail.com>
2011-02-19 18:04:38 -0600

    clean up tests, part 2

--- src/test/java/net/sourceforge/sizeof/Dummy.java
+++
@@ -1,6 +0,0 @@
-package net.sourceforge.sizeof;
-
-public class Dummy {
-    int i;
-    Dummy dummy = null;
-}
--- src/test/java/net/sourceforge/sizeof/TestSizeOf.java
+++ src/test/java/net/sourceforge/sizeof/TestSizeOf.java
@@ -2,12 +2,10 @@ package net.sourceforge.sizeof;
 
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import static junit.framework.Assert.assertEquals;
@@ -19,39 +17,31 @@ import static net.sourceforge.sizeof.SizeOf.*;
 public class TestSizeOf {
     private static final int EMPTY_ARRAY_SIZE = 24;
 
-    // Allows this to be run as a unit test e.g., via mvn test
     @Test
-    public void testSizeOf() throws Exception {
-        main(new String[]{ });
-    }
+    public void testPrimitives() {
+        assertEquals(16, sizeOf(new Object()));
+        assertEquals(16, deepSizeOf(new Object()));
 
-    @Test
-    public void testPrimitives()
-    {
-        assertEquals(16, SizeOf.sizeOf(new Object()));
-        assertEquals(16, SizeOf.deepSizeOf(new Object()));
+        assertEquals(24, sizeOf(new Integer(0)));
+        assertEquals(24, deepSizeOf(new Integer(0)));
 
-        assertEquals(24, SizeOf.sizeOf(new Integer(0)));
-        assertEquals(24, SizeOf.deepSizeOf(new Integer(0)));
+        assertEquals(40, sizeOf(""));
+        assertEquals(40 + EMPTY_ARRAY_SIZE, deepSizeOf(""));
+        assertEquals(40, sizeOf("a"));
+        assertEquals(40 + EMPTY_ARRAY_SIZE + 8, deepSizeOf("a"));
 
-        assertEquals(40, SizeOf.sizeOf(""));
-        assertEquals(40 + EMPTY_ARRAY_SIZE, SizeOf.deepSizeOf(""));
-        assertEquals(40, SizeOf.sizeOf("a"));
-        assertEquals(40 + EMPTY_ARRAY_SIZE + 8, SizeOf.deepSizeOf("a"));
-
-        assertEquals(EMPTY_ARRAY_SIZE, SizeOf.sizeOf(new Object[0]));
+        assertEquals(EMPTY_ARRAY_SIZE, sizeOf(new Object[0]));
         Object[] objects = new Object[100];
-        assertEquals(EMPTY_ARRAY_SIZE + 8 * 100, SizeOf.sizeOf(objects));
-        assertEquals(EMPTY_ARRAY_SIZE + 8 * 100, SizeOf.deepSizeOf(objects));
+        assertEquals(EMPTY_ARRAY_SIZE + 8 * 100, sizeOf(objects));
+        assertEquals(EMPTY_ARRAY_SIZE + 8 * 100, deepSizeOf(objects));
         for(int i = 0; i < objects.length; i++) {
             objects[i] = new Object();
         }
-        assertEquals(EMPTY_ARRAY_SIZE + 8 * 100 + 16 * 100, SizeOf.deepSizeOf(objects));
+        assertEquals(EMPTY_ARRAY_SIZE + 8 * 100 + 16 * 100, deepSizeOf(objects));
     }
 
     @Test
-    public void testCycle()
-    {
+    public void testCycle() {
         Dummy dummy = new Dummy();
         assertEquals(32, sizeOf(dummy));
         assertEquals(32, deepSizeOf(dummy));
@@ -59,78 +49,41 @@ public class TestSizeOf {
         assertEquals(32, deepSizeOf(dummy));
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
-        //SizeOf.turnOnDebug();
-        SizeOf.skipFlyweightObject(true);
-        //SizeOf.setMinSizeToLog(10);
-        SizeOf.setLogOutputStream(new FileOutputStream("log.txt"));
-
-        Integer x = 1;
-        int _int = 0;
-        long _long = 0;
-        String s;
-
-        ArrayList<Integer> aList = new ArrayList<Integer>();
-
-        //print("int: \t\t", iterativeSizeOf(_int));
-        print("int: \t\t", sizeOf(_int));
-        print("long: \t\t", sizeOf(_long));
-        print("char: \t\t", sizeOf('a'));
-        print("double: \t\t", deepSizeOf(0.3D));
-        print("boolean: \t\t", sizeOf(true));
-        print("Integer: \t\t", sizeOf(Integer.valueOf(2)));
-
-        print("empty string: \t", sizeOf(""));
-        print("not empty string: \t", sizeOf("aaaa"));
-        print("not empty string: \t", sizeOf("aaaaaaaa"));
-        //print("empty string: \t", iterativeSizeOf(""));
-        //print("empty array: \t", sizeOf(aList));
-        print("empty list: \t", sizeOf(aList));
-
-        for(int i = 1; i < 10; ++i)
-            aList.add(i);
-
-        //print("10 list: \t", sizeOf(aList));
-        print("10 list: \t", sizeOf(aList));
-        for(int i = 11; i < 20; ++i)
-            aList.add(i);
-        print("20 list no static: \t", sizeOf(aList));
+    @Test
+    public void testInheritance() {
+        assertEquals(24, sizeOf(new Parent()));
+        assertEquals(24, deepSizeOf(new Parent()));
+        assertEquals(32, sizeOf(new Child()));
+        assertEquals(32, deepSizeOf(new Child()));
     }
 
-    public static void print(String msg, long n) {
-        SizeOf.print(msg + humanReadable(n) + "\n");
+    @Test
+    public void testCollections() {
+        assertEquals(144, deepSizeOf(new ArrayList()));
+        assertEquals(216, deepSizeOf(new HashMap()));
+        assertEquals(296, deepSizeOf(new LinkedHashMap()));
+        assertEquals(176, deepSizeOf(new ReentrantReadWriteLock()));
+        assertEquals(192, deepSizeOf(new ConcurrentSkipListMap()));
     }
 
-    public static void heinz() throws IllegalArgumentException, IllegalAccessException, IOException {
-        measureSize(new Object());
-        measureSize(new HashMap());
-        measureSize(new LinkedHashMap());
-        measureSize(new ReentrantReadWriteLock());
-        measureSize(new byte[1000]);
-        measureSize(new boolean[1000]);
-        measureSize(new String("Hello World".toCharArray()));
-        measureSize("Hello World");
-        measureSize(10);
-        measureSize(100);
-        measureSize(1000);
-        measureSize(new Parent());
-        measureSize(new Kid());
-        measureSize(Thread.State.TERMINATED);
-        measureSize(Boolean.TRUE);
+    @Test
+    public void testFlyweights() {
+        skipFlyweightObject(true);
+        assertEquals(0, sizeOf(Thread.State.TERMINATED));
+        assertEquals(0, sizeOf(Boolean.TRUE));
+        assertEquals(0, sizeOf(Integer.valueOf(0)));
     }
 
     private static class Parent {
         private int i;
-        private boolean b;
-        private long l;
     }
 
-    private static class Kid extends Parent {
-        private boolean b;
-        private float f;
+    private static class Child extends Parent {
+        private int j;
     }
 
-    private static void measureSize(Object object) {
-        print(object.getClass().getSimpleName(), deepSizeOf(object));
+    private static class Dummy {
+        int i;
+        Dummy dummy = null;
     }
 }
